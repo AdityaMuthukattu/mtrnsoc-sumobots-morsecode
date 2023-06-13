1bdf517fbf31682f2910adcc2c053bf1a79bdbe3
Aditya M <19423025+AdityaMuthukattu@users.noreply.github.com>
2023-06-13 17:18:33 +1000

    Create morse-starter.ino

---
+++ morse-starter.ino
@@ -0,0 +1,35 @@
+/*
+MTRN Soc Morse Code excercise solution
+
+Written by Aditya Muthukattu on 30/05/2023
+
+*/
+
+int dotDelay = 200;
+const char string[] = "";
+
+// Array of all letters from a to z
+char* letters[] = {
+  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",".---", "-.-", 
+  ".-..", "--", "-.", "---", ".--.", "--.-", ".-.","...", "-", "..-", "...-",
+  ".--", "-..-", "-.--", "--.."
+};
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // your code here
+  
+}
+
+void flashSeq (char* sequence){
+  // your code here
+  
+}
+void flash(char dotDash) {
+  // your code here
+  
+}
